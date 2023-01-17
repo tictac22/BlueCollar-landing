87a9d910740ce2aab01c712679f2e140d294d2fa
tictac <fominsartems947@gmail.com>
2023-01-17 04:11:26 +0200

    delete comments

--- functions/projects-post.php
+++ functions/projects-post.php
@@ -7,33 +7,27 @@ function regiser_servicesPost_types(){
 	register_post_type('projects', [
 		'label'  => null,
 		'labels' => [
-			'name'               => 'projects', // основное название для типа записи
-			'singular_name'      => 'project', // название для одной записи этого типа
-			'add_new'            => 'Add project', // для добавления новой записи
-			'add_new_item'       => 'Add projects', // заголовка у вновь создаваемой записи в админ-панели.
-			'edit_item'          => 'Edit projects', // для редактирования типа записи
-			'new_item'           => 'New projects', // текст новой записи
-			'view_item'          => 'Watch projects', // для просмотра записи этого типа.
-			'search_items'       => 'Search', // для поиска по этим типам записи
-			'not_found'          => 'Not found', // если в результате поиска ничего не было найдено
-			'not_found_in_trash' => 'Not found in trash', // если не было найдено в корзине
-			'menu_name'          => 'projects', // название меню
+			'name'               => 'projects', 
+			'singular_name'      => 'project', 
+			'add_new'            => 'Add project', 
+			'add_new_item'       => 'Add projects', 
+			'edit_item'          => 'Edit projects', 
+			'new_item'           => 'New projects', 
+			'view_item'          => 'Watch projects', 
+			'search_items'       => 'Search', 
+			'not_found'          => 'Not found', 
+			'not_found_in_trash' => 'Not found in trash', 
+			'menu_name'          => 'projects', 
 		],
 		'description'            => '',
-		'public'                 => true,
-		// 'publicly_queryable'  => null, // зависит от public
-		// 'exclude_from_search' => null, // зависит от public
-		// 'show_ui'             => null, // зависит от public
-		// 'show_in_nav_menus'   => null, // зависит от public
-		'show_in_menu'           => null, // показывать ли в меню админки
-		// 'show_in_admin_bar'   => null, // зависит от show_in_menu
-		'show_in_rest'        => null, // добавить в REST API. C WP 4.7
-		'rest_base'           => null, // $post_type. C WP 4.7
+		'public'                 => true,	
+		'show_in_menu'           => null, 	
+		'show_in_rest'        => null, 
+		'rest_base'           => null, 
 		'menu_position'       => null,
 		'menu_icon'           => 'dashicons-admin-generic',
-
 		'hierarchical'        => false,
-		'supports'            => [ 'title','editor','thumbnail' ], // 'title','editor','author','thumbnail','excerpt','trackbacks','custom-fields','comments','revisions','page-attributes','post-formats'
+		'supports'            => [ 'title','editor','thumbnail' ], 
 		'taxonomies'          => [],
 		'has_archive'         => false,
 		'rewrite'             => true,
@@ -47,9 +41,8 @@ function regiser_servicesPost_types(){
 add_action( 'init', 'create_taxonomy' );
 function create_taxonomy(){
 
-	// список параметров: wp-kama.ru/function/get_taxonomy_labels
 	register_taxonomy( 'project_type', [ 'projects' ], [
-		'label'                 => '', // определяется параметром $labels->name
+		'label'                 => '', 
 		'labels'                => [
 			'name'              => 'type',
 			'singular_name'     => 'type',
@@ -65,25 +58,15 @@ function create_taxonomy(){
 			'menu_name'         => 'type',
 			'back_to_items'     => '← Back to type',
 		],
-		'description'           => '', // описание таксономии
-		'public'                => true,
-		// 'publicly_queryable'    => null, // равен аргументу public
-		// 'show_in_nav_menus'     => true, // равен аргументу public
-		// 'show_ui'               => true, // равен аргументу public
-		// 'show_in_menu'          => true, // равен аргументу show_ui
-		// 'show_tagcloud'         => true, // равен аргументу show_ui
-		// 'show_in_quick_edit'    => null, // равен аргументу show_ui
+		'description'           => '', 
+		'public'                => true,	
 		'hierarchical'          => false,
-
-		'rewrite'               => true,
-		//'query_var'             => $taxonomy, // название параметра запроса
+		'rewrite'               => true,		
 		'capabilities'          => array(),
-		'meta_box_cb'           => null, // html метабокса. callback: `post_categories_meta_box` или `post_tags_meta_box`. false — метабокс отключен.
-		'show_admin_column'     => false, // авто-создание колонки таксы в таблице ассоциированного типа записи. (с версии 3.5)
-		'show_in_rest'          => null, // добавить в REST API
-		'rest_base'             => null, // $taxonomy
-		// '_builtin'              => false,
-		//'update_count_callback' => '_update_post_term_count',
+		'meta_box_cb'           => null, 
+		'show_admin_column'     => false, 
+		'show_in_rest'          => null, 
+		'rest_base'             => null, 		
 	] );
 }
 
